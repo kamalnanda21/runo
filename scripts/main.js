af66c7a6fa953affbc1bb3395695be5f63499c0f
Kamal <kamalnanda01@gmail.com>
2024-04-15 11:12:56 +0530

    fixed some issues.

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,5 @@ const navlinks = document.getElementById("navlinks");
 
 hamburger.addEventListener("click", ()=>{
     navlinks.classList.toggle("navlinks-active");
+    hamburger.classList.toggle("cross");
 })
